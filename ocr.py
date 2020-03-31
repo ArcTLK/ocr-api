67371ab9a7707ee12ddad7bc0ff1ba07c77f9605
ArcTLK <adityanair313@gmail.com>
2020-03-31 20:36:43 +0530

    Initial Commit

---
+++ ocr.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import pytesseract
+import cv2
+import os
+
+def getText(image):
+    image = cv2.imread(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    filename = '{}.png'.format(os.getpid())
+    cv2.imwrite(filename, gray)
+    text = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    return text
